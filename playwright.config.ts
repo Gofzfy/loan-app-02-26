7912fd1dafd4d97547a28307b8b017c66fcf71d9
Lesson-5 <reslekkun@gmail.com>
2026-05-11 00:31:38 +0300

    feat: test device changed

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
     /* Test against mobile viewports. */
     {
       name: 'Mobile Chrome',
-      use: { ...devices['Galaxy S24'] },
+      use: { ...devices['Galaxy S23'] },
     },
     // {
     //   name: 'Mobile Safari',
